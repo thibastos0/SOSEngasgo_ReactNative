46834c091877c605401c1320126e4b658526b4b2
Gustavo Martins <gustao_meiramartins@outlook.com>
2026-09-09 15:22:50 -0300

    feat: criado a estrutura inicial da HomeScreen

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { colors } from '../theme/colors';
+
+export default function HomeScreen({ navigation }: any) {
+    return (
+        <View style={styles.container}>
+            {/* Header */}
+            <View style={styles.header}>
+                <Text style={styles.headerLogo}>➕</Text>
+                <Text style={styles.headerTitle}>SOS Engasgo</Text>
+            </View>
+
+            {/* Cartão Central */}
+            <View style={styles.card}>
+                <Text style={styles.cardTitle}>Emergência</Text>
+                <Text style={styles.cardSub}>Em caso de engasgo, pressione o botão abaixo imediatamente</Text>
+
+                <Pressable 
+                    style={styles.botaoAcionar}
+                    onPress={() => navigation.navigate('ConfirmLocation')}
+                >
+                    <Text style={styles.textoAcionar}>ACIONAR</Text>
+                </Pressable>
+
+                <Text style={styles.cardFooter}>Toque para acionar socorro.</Text>
+            </View>
+        </View>
+    );
+}
