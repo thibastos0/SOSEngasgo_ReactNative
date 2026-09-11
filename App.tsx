1de04f343a9b85e519ea8caf8c2c9d7ba4b1be29
Gustavo Martins <gustao_meiramartins@outlook.com>
2026-09-11 11:11:18 -0300

    refactor: remoção do InitialRouteName

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import {createStaticNavigation} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { WelcomeScreen }  from './src/screens/WelcomeScreen';
+import  HomeScreen  from './src/screens/HomeScreen';
 
 
 const RootStack = createNativeStackNavigator({
+
   screens: {
     Welcome: {
       screen: WelcomeScreen,
+      options: {title: 'SOSEngasgo'},
+    },
+    Home: {
+      screen: HomeScreen,
       options: {title: 'SOSEngasgo'},
     },
   },
