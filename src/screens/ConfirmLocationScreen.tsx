a5b70c26c835f1a4114338117d6eb1b01d910b09
wgustw <gustavobravowelke@gmail.com>
2026-09-12 16:49:37 -0300

    feat: adiciona mapa simulado e descrição da localização

--- src/screens/ConfirmLocationScreen.tsx
+++ src/screens/ConfirmLocationScreen.tsx
@@ -4,9 +4,19 @@ import { colors } from '../theme/colors';
 
 export default function ConfirmLocationScreen() {
     return (
-        <View style={styles.container}>
+        <><View style={styles.container}>
             <Text style={styles.title}>Confirmar Localização</Text>
-        </View>
+        </View><Text style={styles.title}>Confirmar Localização</Text><View style={styles.mapBox}>
+                <Text style={styles.mapText}>
+                    [ Simulação do Google Maps ]
+                </Text>
+            </View><Text style={styles.description}>
+                O socorro será enviado para o endereço detectado acima.
+                Confirme para iniciar as instruções de primeiros socorros.
+            </Text></>
+
+
+
     );
 }
 
@@ -23,4 +33,23 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         textAlign: 'center',
     },
+    mapBox: {
+    height: 180,
+    backgroundColor: '#EAEAEA',
+    borderRadius: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 16,
+},
+mapText: {
+    color: colors.textMuted,
+},
+description: {
+    color: colors.textMain,
+    fontSize: 14,
+    lineHeight: 20,
+    textAlign: 'center',
+    marginBottom: 20,
+},
+
 });
